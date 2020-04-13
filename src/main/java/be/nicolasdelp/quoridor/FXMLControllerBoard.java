344853d8002ace6fa31d4df160d1e097c1148903
nicolasdelp <56953219+nicolasdelp@users.noreply.github.com>
2020-04-14 01:01:54 +0200

    FXML Board suite + images murs vertical

--- src/main/java/be/nicolasdelp/quoridor/FXMLControllerBoard.java
+++ src/main/java/be/nicolasdelp/quoridor/FXMLControllerBoard.java
@@ -3,15 +3,33 @@ package be.nicolasdelp.quoridor;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 public class FXMLControllerBoard implements Initializable {
     
     @FXML
     private GridPane grid;
+
+    @FXML
+    private ImageView box28;
+
+    @FXML
+    private ImageView box08;
+
+    @FXML
+    void box28Clicked(MouseEvent event) {
+        final URL imageURL = getClass().getResource("img/pion-vert.png");  
+        final Image image = new Image(imageURL.toExternalForm());
+        box28.setImage(image);
+        final URL imageURL2 = getClass().getResource("img/case.png");  
+        final Image image2 = new Image(imageURL2.toExternalForm());
+        box08.setImage(image2);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
