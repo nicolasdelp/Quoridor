02b59634d2788b90ba692dcf7193fe16977aeda0
nicolasdelp <56953219+nicolasdelp@users.noreply.github.com>
2020-04-18 01:33:15 +0200

    IllegalMovement

--- src/main/java/be/nicolasdelp/quoridor/rules/IllegalMovement.java
+++ src/main/java/be/nicolasdelp/quoridor/rules/IllegalMovement.java
@@ -6,29 +6,71 @@ public class IllegalMovement implements PlayerRule {
 
     @Override
     public void verify(Board board, Player player, Position position) throws RuleViolated{
-        if(position.getX() > player.getPawn().getPosition().getX()+2){ //Si on essaye de bouger de plus de 1 case vers la droite avec le pion
-            throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que de une case à la fois !");
+        if(board.getBoardBoxes()[player.getPawn().getPosition().getX()][player.getPawn().getPosition().getY()+2].getisOccuped()){ //Si la case en haut est occupé par un pion
+            if(board.getBoardBoxes()[player.getPawn().getPosition().getX()][player.getPawn().getPosition().getY()+3].getisOccuped()){ //Si il y a un mur derriere l'adversaire
+                if((position.getX() != player.getPawn().getPosition().getX()-2 && position.getY() != player.getPawn().getPosition().getY()-2) || (position.getX() != player.getPawn().getPosition().getX()+2 && position.getY() != player.getPawn().getPosition().getY()-2) || (position.getX() != player.getPawn().getPosition().getX()-2 && position.getY() != player.getPawn().getPosition().getY()) || (position.getX() != player.getPawn().getPosition().getX()+2 && position.getY() != player.getPawn().getPosition().getY()) || (position.getX() != player.getPawn().getPosition().getX() && position.getY() != player.getPawn().getPosition().getY()+2)){ //Les 5 cases possibles
+                    throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que dans les cases à côté de votre adversaire !");
+                }
+            }
+            else{ //Si il n'y a pas de mur derriere le pion adverse
+
+            }
         }
-        if(position.getX() < player.getPawn().getPosition().getX()-2){ //Si on essaye de bouger de plus de 1 case vers la gauche avec le pion
-            throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que de une case à la fois !");
+        if(board.getBoardBoxes()[player.getPawn().getPosition().getX()][player.getPawn().getPosition().getY()-2].getisOccuped()){ //Si la case en bas est occupé par un pion
+            if(board.getBoardBoxes()[player.getPawn().getPosition().getX()][player.getPawn().getPosition().getY()-3].getisOccuped()){ //Si il y a un mur derriere l'adversaire
+                if((position.getX() != player.getPawn().getPosition().getX()+2 && position.getY() != player.getPawn().getPosition().getY()+2) || (position.getX() != player.getPawn().getPosition().getX()-2 && position.getY() != player.getPawn().getPosition().getY()+2) || (position.getX() != player.getPawn().getPosition().getX()-2 && position.getY() != player.getPawn().getPosition().getY()) || (position.getX() != player.getPawn().getPosition().getX()+2 && position.getY() != player.getPawn().getPosition().getY()) || (position.getX() != player.getPawn().getPosition().getX() && position.getY() != player.getPawn().getPosition().getY()-2)){ //Les 5 cases possibles
+                    throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que dans les cases à côté de votre adversaire !");
+                }
+            }
+            else{ //Si il n'y a pas de mur derriere le pion adverse
+
+            }
         }
-        if(position.getY() > player.getPawn().getPosition().getY()+2){ //Si on essaye de bouger de plus de 1 case vers le haut avec le pion 
-            throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que de une case à la fois !");
+        if(board.getBoardBoxes()[player.getPawn().getPosition().getX()+2][player.getPawn().getPosition().getY()].getisOccuped()){ //Si la case à droite est occupé par un pion
+            if(board.getBoardBoxes()[player.getPawn().getPosition().getX()+3][player.getPawn().getPosition().getY()].getisOccuped()){ //Si il y a un mur derriere l'adversaire
+                if((position.getX() != player.getPawn().getPosition().getX()-2 && position.getY() != player.getPawn().getPosition().getY()+2) || (position.getX() != player.getPawn().getPosition().getX()-2 && position.getY() != player.getPawn().getPosition().getY()-2) || (position.getX() != player.getPawn().getPosition().getX() && position.getY() != player.getPawn().getPosition().getY()-2) || (position.getX() != player.getPawn().getPosition().getX()+2 && position.getY() != player.getPawn().getPosition().getY()) || (position.getX() != player.getPawn().getPosition().getX() && position.getY() != player.getPawn().getPosition().getY()+2)){ //Les 5 cases possibles
+                    throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que dans les cases à côté de votre adversaire !");
+                }
+            }
+            else{ //Si il n'y a pas de mur derriere le pion adverse
+
+            }
         }
-        if(position.getY() < player.getPawn().getPosition().getY()-2){ //Si on essaye de bouger de plus de 1 case vers le bas avec le pion
-            throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que de une case à la fois !");
+        if(board.getBoardBoxes()[player.getPawn().getPosition().getX()-2][player.getPawn().getPosition().getY()].getisOccuped()){ //Si la case à gauche est occupé par un pion
+            if(board.getBoardBoxes()[player.getPawn().getPosition().getX()-3][player.getPawn().getPosition().getY()].getisOccuped()){ //Si il y a un mur derriere l'adversaire
+                if((position.getX() != player.getPawn().getPosition().getX()+2 && position.getY() != player.getPawn().getPosition().getY()+2) || (position.getX() != player.getPawn().getPosition().getX()-2 && position.getY() != player.getPawn().getPosition().getY()+2) || (position.getX() != player.getPawn().getPosition().getX()-2 && position.getY() != player.getPawn().getPosition().getY()) || (position.getX() != player.getPawn().getPosition().getX()+2 && position.getY() != player.getPawn().getPosition().getY()) || (position.getX() != player.getPawn().getPosition().getX() && position.getY() != player.getPawn().getPosition().getY()-2)){ //Les 5 cases possibles
+                    throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que dans les cases à côté de votre adversaire !");
+                }
+            }
+            else{ //Si il n'y a pas de mur derriere le pion adverse
+
+            }
         }
-        if(position.getX() == player.getPawn().getPosition().getX()+2 && position.getY() == player.getPawn().getPosition().getY()+2 ){ //Si le joueur veut aller en diagonal en haut à droite
-            throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que vers le haut, le bas, la droite et la gauche si vous n'êtes pas bloqué !");
-        }
-        if(position.getX() == player.getPawn().getPosition().getX()-2 && position.getY() == player.getPawn().getPosition().getY()+2 ){ //Si le joueur veut aller en diagonal en haut à gauche
-            throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que vers le haut, le bas, la droite et la gauche si vous n'êtes pas bloqué !");
-        }
-        if(position.getX() == player.getPawn().getPosition().getX()+2 && position.getY() == player.getPawn().getPosition().getY()-2 ){ //Si le joueur veut aller en diagonal en bas à droite
-            throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que vers le haut, le bas, la droite et la gauche si vous n'êtes pas bloqué !");
-        }
-        if(position.getX() == player.getPawn().getPosition().getX()-2 && position.getY() == player.getPawn().getPosition().getY()-2 ){ //Si le joueur veut aller en diagonal en bas à gauche
-            throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que vers le haut, le bas, la droite et la gauche si vous n'êtes pas bloqué !");
+        else{
+            if(position.getX() > player.getPawn().getPosition().getX()+2){ //Si on essaye de bouger de plus de 1 case vers la droite avec le pion
+                throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que de une case à la fois !");
+            }
+            if(position.getX() < player.getPawn().getPosition().getX()-2){ //Si on essaye de bouger de plus de 1 case vers la gauche avec le pion
+                throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que de une case à la fois !");
+            }
+            if(position.getY() > player.getPawn().getPosition().getY()+2){ //Si on essaye de bouger de plus de 1 case vers le haut avec le pion 
+                throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que de une case à la fois !");
+            }
+            if(position.getY() < player.getPawn().getPosition().getY()-2){ //Si on essaye de bouger de plus de 1 case vers le bas avec le pion
+                throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que de une case à la fois !");
+            }
+            if(position.getX() == player.getPawn().getPosition().getX()+2 && position.getY() == player.getPawn().getPosition().getY()+2 ){ //Si le joueur veut aller en diagonal en haut à droite
+                throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que vers le haut, le bas, la droite et la gauche si vous n'êtes pas bloqué !");
+            }
+            if(position.getX() == player.getPawn().getPosition().getX()-2 && position.getY() == player.getPawn().getPosition().getY()+2 ){ //Si le joueur veut aller en diagonal en haut à gauche
+                throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que vers le haut, le bas, la droite et la gauche si vous n'êtes pas bloqué !");
+            }
+            if(position.getX() == player.getPawn().getPosition().getX()+2 && position.getY() == player.getPawn().getPosition().getY()-2 ){ //Si le joueur veut aller en diagonal en bas à droite
+                throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que vers le haut, le bas, la droite et la gauche si vous n'êtes pas bloqué !");
+            }
+            if(position.getX() == player.getPawn().getPosition().getX()-2 && position.getY() == player.getPawn().getPosition().getY()-2 ){ //Si le joueur veut aller en diagonal en bas à gauche
+                throw new RuleViolated(player, position, board, "Vous ne pouvez avancer que vers le haut, le bas, la droite et la gauche si vous n'êtes pas bloqué !");
+            }
         }
     }
 }
--- src/main/java/be/nicolasdelp/quoridor/rules/WallBlocksPawn.java
+++ src/main/java/be/nicolasdelp/quoridor/rules/WallBlocksPawn.java
@@ -8,28 +8,28 @@ public class WallBlocksPawn implements PlayerRule {
     public void verify(Board board, Player player, Position position) throws RuleViolated{
         if(player.getPawn().getPosition().getX()-1 > 0){ //Verifie si on est toujours dans le plateau si on bouge à gauche
             if(board.getBoardBox(new Position(player.getPawn().getPosition().getX()-1, player.getPawn().getPosition().getY())).getisOccuped()){ //Verifie si la case à gauche n'est pas occupée
-                if(player.getPawn().getPosition().getX()-2 == position.getX()){ //Si le joueur veut aller à gauche
+                if(player.getPawn().getPosition().getX()-2 >= position.getX()){ //Si le joueur veut aller à gauche
                     throw new RuleViolated(player, position, board, "Vous ne pouvez pas avancez, il y a un mur !");
                 }
             }
         }
         if(player.getPawn().getPosition().getX()+1 < board.getLength()){ //Verifie si on est toujours dans le plateau si on bouge à droite
             if(board.getBoardBox(new Position(player.getPawn().getPosition().getX()+1, player.getPawn().getPosition().getY())).getisOccuped()){ //Verifie si la case à droite n'est pas occupée
-                if(player.getPawn().getPosition().getX()+2 == position.getX()){ //Si le joueur veut aller à droite
+                if(player.getPawn().getPosition().getX()+2 <= position.getX()){ //Si le joueur veut aller à droite
                     throw new RuleViolated(player, position, board, "Vous ne pouvez pas avancez, il y a un mur !");
                 }
             }
         }
         if(player.getPawn().getPosition().getY()-1 > 0){ //Verifie si on est toujours dans le plateau si on bouge en bas
             if(board.getBoardBox(new Position(player.getPawn().getPosition().getX(), player.getPawn().getPosition().getY()-1)).getisOccuped()){ //Verifie si la case du dessous n'est pas occupée
-                if(player.getPawn().getPosition().getY()-2 == position.getY()){ //Si le joueur veut aller en bas
+                if(player.getPawn().getPosition().getY()-2 >= position.getY()){ //Si le joueur veut aller en bas
                     throw new RuleViolated(player, position, board, "Vous ne pouvez pas avancez, il y a un mur !");
                 }
             }
         }
         if(player.getPawn().getPosition().getY()+1 < board.getWidth()){ //Verifie si on est toujours dans le plateau si on bouge en haut
             if(board.getBoardBox(new Position(player.getPawn().getPosition().getX(), player.getPawn().getPosition().getY()+1)).getisOccuped()){ //Verifie si la case du dessus n'est pas occupée
-                if(player.getPawn().getPosition().getY()+2 == position.getY()){ //Si le joueur veut aller en haut
+                if(player.getPawn().getPosition().getY()+2 <= position.getY()){ //Si le joueur veut aller en haut
                     throw new RuleViolated(player, position, board, "Vous ne pouvez pas avancez, il y a un mur !");
                 }
             }
