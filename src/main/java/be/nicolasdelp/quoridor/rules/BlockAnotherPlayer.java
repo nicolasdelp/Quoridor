195b594f01f35e6a81910be5848548db19d3686b
nicolasdelp <56953219+nicolasdelp@users.noreply.github.com>
2020-04-23 16:33:47 +0200

    Algo A*

--- src/main/java/be/nicolasdelp/quoridor/rules/BlockAnotherPlayer.java
+++ src/main/java/be/nicolasdelp/quoridor/rules/BlockAnotherPlayer.java
@@ -6,6 +6,26 @@ public class BlockAnotherPlayer implements WallRule {
 
     @Override
     public void verify(Board board, Player player, Wall wall, Position position) throws RuleViolated{
-        //Algorithme A*
+        Board newBoard = board;
+        boolean Stop = false;
+        for(int i = 0; i < newBoard.players.length; i++){
+            if(newBoard.players[i] == newBoard.players[0]){
+                Position source = newBoard.players[0].getPawn().getPosition();
+                int xDestination = 16;
+                Position[] aExplorer = new Position[4];
+                Position[] dejaExplorer = new Position[81]; //81 = 9*9
+                dejaExplorer[0] = newBoard.players[0].getPawn().getPosition();
+                for(int x = 0; x < aExplorer.length; x++){
+                    for(int y = 0; y < aExplorer.length; y++){
+                        if(aExplorer[x].getX() == xDestination){
+                            Stop = true;
+                        }
+                    }
+                }
+                while(!Stop){
+                    
+                }
+            }
+        }
     }
 }
